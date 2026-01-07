fca6d1a2b5546880b55d2ae94624947d0b23e57c
Swapnil Kumbhar <swapnilkumbhar366@gmail.com>
2026-01-07 12:29:16 +0530

    API URL created inside the function.

--- js/currency-converter-script.js
+++ js/currency-converter-script.js
@@ -1,8 +1,5 @@
-
-
 const fromDropDown = document.getElementById('from-currency-select');
 const toDropDown = document.getElementById('to-currency-select');
-
 
 // create Dropdown
 currencies.forEach(currency => {
@@ -23,7 +20,6 @@ currencies.forEach(currency => {
 fromDropDown.value = "USD";
 toDropDown.value = "INR";
 
-
 let convertCurrency = () => {
   const amount = document.querySelector("#amount").value;
   const result = document.querySelector('.result');
@@ -36,18 +32,16 @@ let convertCurrency = () => {
       .then((resp) => resp.json())
       .then((data) => {
         
-        result.innerHTML = `${data.amount} ${data.base} = ${data.convertedAmount} ${data.target}`;
+        let fromExchangeRate = data.conversion_rates[fromCurrency];
+        let toExchangeRate = data.conversion_rates[toCurrency];
 
+        const convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
+        result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`;
       });
   } else {
     alert("Please fill the Amount");
   }
 }
 
-
 document.querySelector('.convert-button').addEventListener("click", convertCurrency);
 window.addEventListener("load", convertCurrency);
-
-
-
-
--- netlify/funnctions/currency.js
+++ netlify/funnctions/currency.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-  const { base, target, amount } = event.queryStringParameters;
+  const { base } = event.queryStringParameters;
 
   const API_KEY = process.env.EXCHANGERATE_API_KEY;
   const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${base}`;
@@ -8,30 +8,16 @@ exports.handler = async (event) => {
     const response = await fetch(url);
     const data = await response.json();
 
-    if (data.result !== "success") {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: "Invalid API response" }),
-      };
-    }
-
-    const rate = data.conversion_rates[target];
-    const convertedAmount = (amount * rate).toFixed(2);
-
     return {
       statusCode: 200,
       body: JSON.stringify({
-        base,
-        target,
-        rate,
-        amount,
-        convertedAmount,
-      }),
+        conversion_rates: data.conversion_rates
+      })
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Currency conversion failed" }),
+      body: JSON.stringify({ error: "Currency conversion failed" })
     };
   }
 };
